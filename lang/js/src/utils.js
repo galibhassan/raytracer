8a6d1aa3ecf1e60e73832b35b69aec314cef7c4a
Galib Hassan <galib.hassan.0@gmail.com>
2020-09-11 23:54:37 +0200

    Fix typo in cramer's rool

--- lang/js/src/utils.js
+++ lang/js/src/utils.js
@@ -75,10 +75,10 @@ const doesHitSphere = (ray, sphere) => {
 }
 
 const cramersRule3 = (A, B, C, I) => {
-    const D = A.x * (B.y * C.z - C.y * B.z) + B.x * (C.y * A.z - A.y * c.z) + C.x * (A.y * B.z - B.y * A.z)
-    const Da = I.x * (B.y * C.z - C.y * B.z) + B.x * (C.y * I.z - I.y * c.z) + C.x * (I.y * B.z - B.y * I.z)
-    const Db = A.x * (I.y * C.z - C.y * I.z) + I.x * (C.y * A.z - A.y * c.z) + C.x * (A.y * I.z - I.y * A.z)
-    const Dc = A.x * (B.y * I.z - I.y * B.z) + B.x * (I.y * A.z - A.y * c.z) + I.x * (A.y * B.z - B.y * A.z)
+    const D = A.x * (B.y * C.z - C.y * B.z) + B.x * (C.y * A.z - A.y * C.z) + C.x * (A.y * B.z - B.y * A.z)
+    const Da = I.x * (B.y * C.z - C.y * B.z) + B.x * (C.y * I.z - I.y * C.z) + C.x * (I.y * B.z - B.y * I.z)
+    const Db = A.x * (I.y * C.z - C.y * I.z) + I.x * (C.y * A.z - A.y * C.z) + C.x * (A.y * I.z - I.y * A.z)
+    const Dc = A.x * (B.y * I.z - I.y * B.z) + B.x * (I.y * A.z - A.y * C.z) + I.x * (A.y * B.z - B.y * A.z)
 
     return {
         a: Da / D,
