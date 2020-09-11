d6b4c5b8ff128a813ec3c28e0ee745e400a57abc
Galib Hassan <galib.hassan.0@gmail.com>
2020-09-11 07:06:30 -0700

    Add cramers rule for 3eq3unknwn

--- lang/js/src/utils.js
+++ lang/js/src/utils.js
@@ -74,7 +74,20 @@ const doesHitSphere = (ray, sphere) => {
     }
 }
 
-module.exports = { color, doesHitSphere }
+const cramersRule3 = (A, B, C, I) => {
+    const D = A.x * (B.y * C.z - C.y * B.z) + B.x * (C.y * A.z - A.y * c.z) + C.x * (A.y * B.z - B.y * A.z)
+    const Da = I.x * (B.y * C.z - C.y * B.z) + B.x * (C.y * I.z - I.y * c.z) + C.x * (I.y * B.z - B.y * I.z)
+    const Db = A.x * (I.y * C.z - C.y * I.z) + I.x * (C.y * A.z - A.y * c.z) + C.x * (A.y * I.z - I.y * A.z)
+    const Dc = A.x * (B.y * I.z - I.y * B.z) + B.x * (I.y * A.z - A.y * c.z) + I.x * (A.y * B.z - B.y * A.z)
+
+    return {
+        a: Da / D,
+        b: Db / D,
+        c: Dc / D
+    }
+}
+
+module.exports = { color, doesHitSphere, cramersRule3 }
 
 // test
 // const ray = new Ray(new Vec3(0,0,0), new Vec3(0,0,1))
